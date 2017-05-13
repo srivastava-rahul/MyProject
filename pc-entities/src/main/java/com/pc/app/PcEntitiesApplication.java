342624fcad3df0f57d017634d4f2259f55bb9506
Parveen Mangla <parveen@recstech.com>
2017-05-13 14:26:47 +0530

    push madi

--- pc-entities/src/main/java/com/pc/app/PcEntitiesApplication.java
+++ pc-entities/src/main/java/com/pc/app/PcEntitiesApplication.java
@@ -2,6 +2,11 @@ package com.pc.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//parveen
+
+
+
+
 
 @SpringBootApplication
 public class PcEntitiesApplication {
