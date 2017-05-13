a8ba3868bcc95523e87cdc4d4fedfcbf2136c334
Vipul Jain <vipul@recstech.com>
2017-05-13 14:29:03 +0530

    ADD  COMENT

--- pc-entities/src/main/java/com/pc/app/PcEntitiesApplication.java
+++ pc-entities/src/main/java/com/pc/app/PcEntitiesApplication.java
@@ -2,6 +2,7 @@ package com.pc.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//ADD TOKEN
 
 @SpringBootApplication
 public class PcEntitiesApplication {
